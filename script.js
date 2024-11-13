3f5a128ae4ee181276f1487d32bab2ced74cfb2d
isaacnewtonthe2nd <ayotuns73@gmail.com>
2024-11-12 18:11:44 -0800

    Create function generateGrid that generates a user chosen grid size.

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 let container = document.querySelector(".grid-container");
 
 let btn = document.querySelector("button");
-btn.addEventListener("click");
+btn.addEventListener("click", () => {
+  let gridDimensions = prompt("Enter your grid dimensions: \nMAX: 100.");
+
+  generateGrid(gridDimensions);
+});
+
+function generateGrid (dimensions) {
+  let sumOfDivs = dimensions ** 2;
+  let divsHeightWidth = (960 / dimensions) - 2;
+
+  for (let i = 0; i < sumOfDivs; i++) {
+    const divSquare = document.createElement("div");
+    divSquare.style.cssText = `border: 1px solid black; width: ${divsHeightWidth}px; height: ${divsHeightWidth}px;`;
+    container.appendChild(divSquare);
+  }
+}
