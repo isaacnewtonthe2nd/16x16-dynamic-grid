60e386dc719e41723e2e82a12f72f1aaf8dcdabf
isaacnewtonthe2nd <ayotuns73@gmail.com>
2024-11-12 18:43:46 -0800

    Create a function gridColors to add random colors to grid when hovered over.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let btn = document.querySelector("button");
 btn.addEventListener("click", () => {
   let gridDimensions = prompt("Enter your grid dimensions: \nMAX: 100.");
 
-  generateGrid(gridDimensions);
+  gridDimensions < 100 ? generateGrid(gridDimensions) : alert("MAX LIMIT! Enter a number less than 100.");
 });
 
 function generateGrid (dimensions) {
@@ -14,6 +14,25 @@ function generateGrid (dimensions) {
   for (let i = 0; i < sumOfDivs; i++) {
     const divSquare = document.createElement("div");
     divSquare.style.cssText = `border: 1px solid black; width: ${divsHeightWidth}px; height: ${divsHeightWidth}px;`;
+    divSquare.addEventListener("mouseenter", () => {
+      gridColors(divSquare);
+    });
     container.appendChild(divSquare);
+  }
+}
+
+function gridColors (element) {
+  let randomNumber = Math.round(Math.random() * 10);
+
+  if (randomNumber <= 2) {
+    element.style.backgroundColor = "orangered";
+  } else if (randomNumber <= 4) {
+    element.style.backgroundColor = "rebeccapurple";
+  } else if (randomNumber <= 6) {
+    element.style.backgroundColor = "turquoise";
+  } else if (randomNumber <= 8) {
+    element.style.backgroundColor = "stalegray";
+  } else if (randomNumber <= 10) {
+    element.style.backgroundColor = "navajowhite";
   }
 }
